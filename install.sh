9364b8744401b47707057cf1639c194d7a71c605
root <s19073@tokyo.kosen-ac.jp>
2020-11-27 06:04:58 +0000

    Fix

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 echo "Create dotfile links"
 for dotfile in $SCRIPT_PATH/.??*; do
     [[ ${dotfile##*/} = ".git" ]] && continue
-    ln -snfv "$dotfile" ${dotfile##*/}
+    ln -snfv "$dotfile" "$HOME/${dotfile##*/}"
 done
 echo "Finish"
