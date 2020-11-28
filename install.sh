8da1110186d66956124ae4ecfd7b7ce40bb28277
root <s10973@tokyo.kosen-ac.jp>
2020-11-28 09:48:04 +0000

    Update

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
+IGNORE_CASE="^\.git"
 SCRIPT_PATH="$(cd $(dirname $0); pwd)"
 
 echo "Create dotfile links"
 for dotfile in $SCRIPT_PATH/.??*; do
-    [[ ${dotfile##*/} = ".git" ]] && continue
+    echo ${dotfile##*/} | grep -sq $IGNORE_CASE && continue
     ln -snfv "$dotfile" "$HOME/${dotfile##*/}"
 done
 echo "Finish"
