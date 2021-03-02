49c158e1bc8ef185e27322a64dfc3d2ee2edf3ab
keke1008 <s19073@tokyo.kosen-ac.jp>
2021-03-03 08:31:50 +0900

    [Update] install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-IGNORE_CASE="^\.git"
-SCRIPT_PATH="$(cd $(dirname $0); pwd)"
+SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
-echo "Create dotfile links"
-for dotfile in $SCRIPT_PATH/.??*; do
-    echo ${dotfile##*/} | grep -sq $IGNORE_CASE && continue
-    ln -snfv "$dotfile" "$HOME/${dotfile##*/}"
-done
-echo "Finish"
+ls -A1 ${SCRIPT_DIR} | grep -vE "^([^.]|\.git)" | xargs -I FILENAME ln -snfv ${SCRIPT_DIR}/FILENAME ${HOME}/temp/FILENAME
