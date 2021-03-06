0ac73210c2ab661c4f8f41c83dd195e1fa415901
keke1008 <s19073@tokyo.kosen-ac.jp>
2021-03-06 09:44:20 +0900

    [fix] link path bug.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 
-ls -A1 ${SCRIPT_DIR} | grep -vE "^([^.]|\.git)" | xargs -I FILENAME ln -snfv ${SCRIPT_DIR}/FILENAME ${HOME}/temp/FILENAME
+ls -A1 ${SCRIPT_DIR} | grep -vE "^([^.]|\.git)" | xargs -I FILENAME ln -snfv ${SCRIPT_DIR}/FILENAME ${HOME}/FILENAME
