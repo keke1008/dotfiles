91bf92a6596594babb293f73dd38dcd714452cc7
keke1008 <keke2222337@gmail.com>
2021-12-17 16:45:27 +0900

    Fix layout

--- xdg_config_home/nvim/lua/commands.lua
+++ xdg_config_home/nvim/lua/commands.lua
@@ -2,13 +2,18 @@ vim.cmd"command Layout lua require'commands'.layout()"
 
 return {
   layout = function ()
+    local trouble = require'trouble'
+    local nvim_tree = require'nvim-tree'
+
+    trouble.close()
+    nvim_tree.close()
     local win_id = vim.api.nvim_get_current_win()
 
     if #vim.lsp.get_active_clients() >= 1 then
-      require'trouble'.open()
+      trouble.open()
     end
 
-    require'nvim-tree'.open()
+    nvim_tree.open()
 
     vim.api.nvim_set_current_win(win_id)
   end
