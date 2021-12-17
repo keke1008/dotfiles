437f635538a24d5f030372ab9b68cdbcaf60ae94
keke1008 <keke2222337@gmail.com>
2021-12-17 14:52:52 +0900

    Improve neovim start time

--- xdg_config_home/nvim/init.lua
+++ xdg_config_home/nvim/init.lua
@@ -36,6 +36,22 @@ vim.o.incsearch = true
 
 vim.g.mapleader = require'utils'.esc'<Space>'
 
+-- https://github.com/neovim/neovim/issues/9570#issuecomment-626275405
+if vim.fn.executable('win32yank.exe') == 1 then
+  vim.g.clipboard = {
+    name= 'win32yank',
+    copy= {
+      ['+']= 'win32yank.exe -i --crlf',
+      ['*']= 'win32yank.exe -i --crlf',
+    },
+    paste= {
+      ['+']= 'win32yank.exe -o --lf',
+      ['*']= 'win32yank.exe -o --lf',
+    },
+    cache_enabled= 0,
+  }
+end
+
 vim.cmd'autocmd TextYankPost * silent! lua vim.highlight.on_yank { timeout = 200 }'
 
 require'plugins'
