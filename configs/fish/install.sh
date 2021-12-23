5f20245feed37aa76bf32f4a64fff38909164f03
keke1008 <keke2222337@gmail.com>
2021-12-24 00:00:20 +0900

    Add install script

--- configs/fish/install.sh
+++ configs/fish/install.sh
@@ -4,3 +4,6 @@ fish_dir=$XDG_CONFIG_HOME/fish
 
 mkdir -p $fish_dir
 ln -snfv $1/config.fish $fish_dir
+
+mkdir -p $fish_dir/functions
+find $1/functions -type f | xargs ln -snfv -t $fish_dir/functions
