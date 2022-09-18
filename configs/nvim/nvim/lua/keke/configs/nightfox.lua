d800c0d38f2388217f2761b96f99e731909a8514
keke1008 <keke2222337@gmail.com>
2022-09-18 23:49:15 +0900

    Change colorscheme

--- configs/nvim/nvim/lua/keke/configs/nightfox.lua
+++ configs/nvim/nvim/lua/keke/configs/nightfox.lua
@@ -1,1 +1,9 @@
 vim.cmd 'colorscheme nightfox'
+
+vim.api.nvim_create_autocmd('VimEnter', {
+    pattern = '*',
+    once = true,
+    callback = function()
+        vim.api.nvim_set_hl(0, 'Visual', { bg = "#385275" })
+    end
+})
