31bbe65c454de0c73adb21a59569d8e4fc7491c9
keke1008 <keke2222337@gmail.com>
2022-09-23 12:22:17 +0900

    Change prompt

--- configs/fish/functions/fish_prompt.fish
+++ configs/fish/functions/fish_prompt.fish
@@ -1,23 +1,36 @@
 function fish_prompt
     set last_pipestatus $pipestatus
 
-    set prompt_bg_job \uf0ae' '(jobs -cl | sed -n '1p')
 
-    set prompt_git \ue725' '(git branch --show-current ^ /dev/null)
+    set path (prompt_pwd --dir-length 3)
+    if string match --quiet '/*' $path
+        set prompt_path '/' (string split --no-empty '/' $path)
+    else
+        set prompt_path (string split '/' $path)
+    end
+
+    set prompt_git \ue725' '(fish_git_prompt '%s')
+
+    set prompt_bg_job (jobs --command | tail -n +1 | tac)
 
     set prompt_status
-    string match -qvr '^(0|141)$' $last_pipestatus
-    and set prompt_status (__fish_pipestatus_with_signal $last_pipestatus | string join '|')
+    for st in $last_pipestatus
+        if not contains $st 0 141
+            set prompt_status $last_pipestatus
+            break
+        end
+    end
 
     set prompt_suffix '$'
-    contains $USER root && set prompt_suffix '#'
+    fish_is_root_user && set prompt_suffix '#'
 
     echo ''
     light_prompt -f brwhite \
-        -b 666666 (string replace -r "^$HOME" '~' (pwd)) \
+        -b 5a5a5a $prompt_path \
         -b 368888 $prompt_git \
-        -b 888d31 $prompt_bg_job \
+        -b 888d31 $prompt_bg_job
 
     light_prompt -f brwhite \
-        -b 881616 $prompt_status -b brblack $prompt_suffix
+        -b 881616 $prompt_status \
+        -b brblack $prompt_suffix
 end
