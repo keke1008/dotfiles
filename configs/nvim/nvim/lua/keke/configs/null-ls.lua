bfd178d43cad5f585a69ad788ae1a3852ea6fa15
keke1008 <keke2222337@gmail.com>
2022-10-30 12:43:35 +0900

    Add configs for null-ls

--- configs/nvim/nvim/lua/keke/configs/null-ls.lua
+++ configs/nvim/nvim/lua/keke/configs/null-ls.lua
@@ -1,4 +1,5 @@
 local null_ls = require("null-ls")
+local code_actions = null_ls.builtins.code_actions
 local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
 
@@ -12,7 +13,12 @@ end
 
 null_ls.setup({
     sources = {
+        -- lua
         diagnostics.luacheck.with(mason_ready("luacheck")),
         formatting.stylua.with(mason_ready("stylua")),
+
+        -- js/ts/..
+        code_actions.eslint_d.with(mason_ready("eslint_d")),
+        formatting.prettierd.with(mason_ready("prettierd")),
     },
 })
