285f63d498ce76e1b33a969f8781533add7eaac3
keke1008 <keke2222337@gmail.com>
2022-12-25 22:53:44 +0900

    feat(neovim): compress keymap

--- configs/nvim/nvim/lua/keke/keymap.lua
+++ configs/nvim/nvim/lua/keke/keymap.lua
@@ -2,6 +2,24 @@ local menu = require("keke.side_menu")
 
 local remap = vim.keymap.set
 
+remap("n", "mq", "q")
+remap("n", "m<S-q>", "<S-q>")
+remap("n", "m<C-q>", "<C-q>")
+
+local function compress(key)
+    local prefix = "m"
+    local targets = { key, ("<S-%s>"):format(key), ("<C-%s>"):format(key), ("<M-%s>"):format(key) }
+    for _, target in ipairs(targets) do
+        remap("n", prefix .. target, target)
+    end
+end
+
+compress("q")
+compress("r")
+compress("t")
+compress("@")
+compress("m")
+
 remap("n", "j", "gj")
 remap("n", "k", "gk")
 remap("n", "<Esc>", "<CMD>write<CR>")
