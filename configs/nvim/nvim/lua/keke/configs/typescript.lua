f5c99eb11dacbfd20375b4032c69d5b1e5ee223a
keke1008 <keke2222337@gmail.com>
2023-02-01 21:37:25 +0900

    fix(neovim): do not launch tsserver in deno project

--- configs/nvim/nvim/lua/keke/configs/typescript.lua
+++ configs/nvim/nvim/lua/keke/configs/typescript.lua
@@ -1,10 +1,13 @@
-local root_pattern = require("lspconfig.util").root_pattern
+local util = require("lspconfig.util")
 local typescript = require("typescript")
 
 local lsp = require("keke.utils.lsp")
 
+local is_deno_project = util.root_pattern("deno.json", "deno.jsonc")(vim.fn.getcwd())
+if is_deno_project then return end
+
 typescript.setup({
     server = lsp.extend_default_config({
-        root_dir = root_pattern("package.json"),
+        root_dir = util.find_package_json_ancestor,
     }),
 })
