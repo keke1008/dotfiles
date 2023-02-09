b256c4200bb9fe404510ff895c1a03b23c98d498
keke1008 <keke2222337@gmail.com>
2023-02-09 17:27:35 +0900

    fix(neovim) run rust-analyzer if codelldb is not installed

--- configs/nvim/nvim/lua/keke/configs/rust-tools.lua
+++ configs/nvim/nvim/lua/keke/configs/rust-tools.lua
@@ -1,24 +1,11 @@
 -- You should not install 'rust_analyzer' with nvim-lsp-installer,
 -- because rust-tools will automatically enable this lsp.
 
-local map = require("keke.utils.mapping")
-
 local rt = require("rust-tools")
 local rt_dap = require("rust-tools.dap")
 local mason_registry = require("mason-registry")
 local lsp = require("keke.utils.lsp")
-
-vim.api.nvim_set_hl(0, "rustInlayHints", { fg = "#3467af" })
-
-local installed, codelldb = pcall(mason_registry.get_package, "codelldb")
-if not installed then
-    vim.notify("`codelldb` in not installed.", vim.log.levels.INFO)
-    return
-end
-
-local package_path = codelldb:get_install_path()
-local codelldb_path = package_path .. "/extension/adapter/codelldb"
-local liblldb_path = package_path .. "/extension/lldb/lib/liblldb.so"
+local map = require("keke.utils.mapping")
 
 rt.setup({
     server = lsp.extend_default_config({
@@ -40,7 +27,13 @@ rt.setup({
             other_hints_prefix = " ", -- \uf554
         },
     },
-    dap = {
-        adapter = rt_dap.get_codelldb_adapter(codelldb_path, liblldb_path),
-    },
+    dap = (function()
+        if not mason_registry.is_installed("codelldb") then return end
+
+        local install_path = mason_registry.get_package("codelldb"):get_install_path()
+        local codelldb_path = install_path .. "/extension/adapter/codelldb"
+        local liblldb_path = install_path .. "/extension/lldb/lib/liblldb.so"
+
+        return { adapter = rt_dap.get_codelldb_adapter(codelldb_path, liblldb_path) }
+    end)(),
 })
