919cb3ebd005026f07fffba42d16151ccdd1791a
keke1008 <keke2222337@gmail.com>
2023-02-10 11:08:40 +0900

    fix(neovim): substitute keymap

--- configs/nvim/nvim/lua/keke/configs/substitute.lua
+++ configs/nvim/nvim/lua/keke/configs/substitute.lua
@@ -6,8 +6,6 @@ local function substitute(command)
     return function() require("substitute")[command]() end
 end
 
-map.add_group(map.l2("su"), "substitute")
-
 vim.keymap.set("n", map.l2("su"), substitute("operator"), { desc = "substitute" })
 vim.keymap.set("n", map.l2("suu"), substitute("line"), { desc = "substitute line" })
 vim.keymap.set("n", map.l2("sU"), substitute("eol"), { desc = "substitute eol" })
