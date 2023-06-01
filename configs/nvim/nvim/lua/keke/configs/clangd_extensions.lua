1f99bae2afff203be2261bdaab7b201efec9f4db
keke1008 <keke2222337@gmail.com>
2023-06-01 17:43:50 +0900

    feat(neovim): add clang keymap

--- configs/nvim/nvim/lua/keke/configs/clangd_extensions.lua
+++ configs/nvim/nvim/lua/keke/configs/clangd_extensions.lua
@@ -2,5 +2,10 @@ local clangd_extensions = require("clangd_extensions")
 local lsp = require("keke.utils.lsp")
 
 clangd_extensions.setup({
-    server = lsp.default_config,
+    server = lsp.extend_default_config({
+        on_attach = function(_, bufnr)
+            local opts = { buffer = bufnr, silent = true }
+            vim.keymap.set("n", "<leader>ls", "<CMD>ClangdSwitchSourceHeader<CR>", opts)
+        end,
+    }),
 })
