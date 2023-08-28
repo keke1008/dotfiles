3ae942beafd4034b08de9289f2da10355a5c94ae
keke1008 <keke2222337@gmail.com>
2023-08-28 21:31:29 +0900

    feat(nvim): change completion behavior

--- configs/nvim/nvim/lua/keke/configs/cmp.lua
+++ configs/nvim/nvim/lua/keke/configs/cmp.lua
@@ -53,7 +53,7 @@ cmp.setup({
         { name = "treesitter" },
     })),
     completion = {
-        completeopt = "menuone,noinsert",
+        completeopt = "menuone",
     },
     mapping = {
         ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), remap_mode),
