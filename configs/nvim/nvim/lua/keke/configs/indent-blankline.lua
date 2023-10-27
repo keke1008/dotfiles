f065b6911ea02092cd608d3e6d3cf5c753e3e141
keke1008 <49900846+keke1008@users.noreply.github.com>
2023-10-27 20:37:54 +0900

    fix(nvim): indent-blankline

--- configs/nvim/nvim/lua/keke/configs/indent-blankline.lua
+++ configs/nvim/nvim/lua/keke/configs/indent-blankline.lua
@@ -31,4 +31,6 @@ vim.opt.list = true
 vim.opt.listchars:append("space:⋅")
 vim.opt.listchars:append("eol:↴")
 
-require("ibl").setup()
+require("ibl").setup({
+    scope = { show_start = false, show_end = false },
+})
