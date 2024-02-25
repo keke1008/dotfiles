fe4480a9a347a56e240f846126dc937027f6a039
keke1008 <keke2222337@gmail.com>
2024-02-25 18:12:12 +0900

    feat(nvim): update

--- configs/nvim/nvim/init.lua
+++ configs/nvim/nvim/init.lua
@@ -1,21 +1,3 @@
-vim.g.did_install_default_menus = 1
-vim.g.did_install_syntax_menu = 1
-vim.g.did_indent_on = 1
-vim.g.loaded_2html_plugin = 1
-vim.g.loaded_gzip = 1
-vim.g.loaded_man = 1
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_remote_plugins = 1
-vim.g.loaded_shada_plugin = 1
-vim.g.loaded_spellfile_plugin = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_tutor_mode_plugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.skip_loading_mswin = 1
-
 vim.o.number = true
 vim.o.cursorline = true
 vim.o.cursorcolumn = true
@@ -86,5 +68,4 @@ vim.api.nvim_create_user_command("WipeInvisibleBuffers", function()
     end
 end, { desc = "Wipe invisible buffers" })
 
--- require("keke.plugins")
 require("keke.lazy")
