a6709cb7380a4783d7359d5a9fda8aba23220439
keke1008 <keke2222337@gmail.com>
2024-03-09 13:17:16 +0900

    feat: update installation script

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # dotfiles
 
 ![installation test](https://github.com/keke1008/dotfiles/actions/workflows/installation.yaml/badge.svg)
+
+## Installation
+
+```sh
+$ git clone https://github.com/keke1008/dotfiles.git
+$ cd dotfiles
+$ ./install.sh all
+```
--- install.sh
+++ install.sh
@@ -5,6 +5,29 @@ export XDG_CONFIG_HOME=$HOME/.config
 
 echo $DOTPATH > $HOME/.dotpath
 
-for dir in $DOTPATH/configs/* ; do
-  "$dir/install.sh"
+if [ $# -eq 0 ]; then
+    echo "Usage: $0 [ all | nvim | fish | ... ] [ nvim | fish | ... ] ..."
+    exit 1
+fi
+
+# check if the given configuration directories are valid
+for dir in $@; do
+    if [ $dir = "all" ]; then
+        continue
+    fi
+    if [ ! -x $DOTPATH/configs/$dir/install.sh ]; then
+        echo "Error: $dir is not a valid configuration directory"
+        exit 1
+    fi
+done
+
+# install the given directories
+for dir in $@; do
+    if [ $dir = "all" ]; then
+        for d in $DOTPATH/configs/*; do
+            $d/install.sh
+        done
+    else
+        $DOTPATH/configs/$dir/install.sh
+    fi
 done
