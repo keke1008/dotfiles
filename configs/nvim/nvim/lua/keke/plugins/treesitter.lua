da72a4522caccf4528bbc8bfc4a83fdb0310cafd
keke1008 <keke2222337@gmail.com>
2024-03-09 12:46:47 +0900

    fix(nvim): treesitter plugin

--- configs/nvim/nvim/lua/keke/plugins/treesitter.lua
+++ configs/nvim/nvim/lua/keke/plugins/treesitter.lua
@@ -1,71 +1,73 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    event = { "BufRead", "BufNewFile" },
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        "nvim-treesitter/nvim-treesitter-context",
-        "andymass/vim-matchup",
-        "RRethy/nvim-treesitter-endwise",
-        "windwp/nvim-ts-autotag",
-    },
-    main = "nvim-treesitter.configs",
-    opts = {
-        highlight = { enable = true },
-        matchup = { enable = true },
-        endwise = { enable = true },
-
-        indent = {
-            enable = true,
-            disable = { "cpp", "python" },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            "nvim-treesitter/nvim-treesitter-context",
+            "andymass/vim-matchup",
+            "RRethy/nvim-treesitter-endwise",
+            "windwp/nvim-ts-autotag",
         },
+        event = { "BufRead", "BufNewFile" },
+        main = "nvim-treesitter.configs",
+        opts = {
+            highlight = { enable = true },
+            matchup = { enable = true },
+            endwise = { enable = true },
 
-        textobjects = {
-            select = {
+            indent = {
                 enable = true,
-                lookahead = true,
+                disable = { "cpp", "python" },
+            },
+
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        ["ia"] = "@parameter.inner",
+                        ["aa"] = "@parameter.outer",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                        ["ab"] = "@block.outer",
+                        ["ib"] = "@block.inner",
+                        ["al"] = "@call.outer",
+                        ["il"] = "@call.inner",
+                        ["ao"] = "@conditional.outer",
+                        ["io"] = "@conditional.inner",
+                        ["as"] = "@statement.outer",
+                    },
+                },
+                swap = {
+                    enable = true,
+                    swap_next = {
+                        ["gsa"] = "@parameter.inner",
+                        ["gsf"] = "@function.outer",
+                        ["gsc"] = "@class.outer",
+                    },
+                    swap_previous = {
+                        ["gSa"] = "@parameter.inner",
+                        ["gSf"] = "@function.outer",
+                        ["gSc"] = "@class.outer",
+                    },
+                },
+            },
+
+            incremental_selection = {
+                enable = true,
                 keymaps = {
-                    ["ia"] = "@parameter.inner",
-                    ["aa"] = "@parameter.outer",
-                    ["af"] = "@function.outer",
-                    ["if"] = "@function.inner",
-                    ["ac"] = "@class.outer",
-                    ["ic"] = "@class.inner",
-                    ["ab"] = "@block.outer",
-                    ["ib"] = "@block.inner",
-                    ["al"] = "@call.outer",
-                    ["il"] = "@call.inner",
-                    ["ao"] = "@conditional.outer",
-                    ["io"] = "@conditional.inner",
-                    ["as"] = "@statement.outer",
+                    init_selection = "v;",
+                    node_incremental = ";",
+                    scope_incremental = "g;",
+                    node_decremental = "+",
                 },
             },
-            swap = {
+
+            autotag = {
                 enable = true,
-                swap_next = {
-                    ["gsa"] = "@parameter.inner",
-                    ["gsf"] = "@function.outer",
-                    ["gsc"] = "@class.outer",
-                },
-                swap_previous = {
-                    ["gSa"] = "@parameter.inner",
-                    ["gSf"] = "@function.outer",
-                    ["gSc"] = "@class.outer",
-                },
             },
-        },
-
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "v;",
-                node_incremental = ";",
-                scope_incremental = "g;",
-                node_decremental = "+",
-            },
-        },
-
-        autotag = {
-            enable = true,
         },
     },
     {
