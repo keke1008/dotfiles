06969a31733ab9a8edd658c3bc21bfeff7b40f68
keke1008 <keke2222337@gmail.com>
2024-03-12 21:14:50 +0900

    feat(nvim): update

--- configs/nvim/nvim/lua/keke/plugins/ai.lua
+++ configs/nvim/nvim/lua/keke/plugins/ai.lua
@@ -12,24 +12,11 @@ return {
             "folke/trouble.nvim",
             "nvim-telescope/telescope.nvim"
         },
-        keys = function()
-            local mode = { "n", "v" }
+        event = "VeryLazy",
+        opts = function()
             return {
-                { "<leader>cc", "ChatGPTCompleteCode",                  mode = mode, desc = "CompleteCode" },
-                { "<leader>ce", "ChatGPTEditWithInstruction",           mode = mode, desc = "Edit with instruction" },
-                { "<leader>cg", "ChatGPTRun grammar_correction",        mode = mode, desc = "Grammar Correction" },
-                { "<leader>ct", "ChatGPTRun translate",                 mode = mode, desc = "Translate" },
-                { "<leader>ck", "ChatGPTRun keywords",                  mode = mode, desc = "Keywords" },
-                { "<leader>cd", "ChatGPTRun docstring",                 mode = mode, desc = "Docstring" },
-                { "<leader>ca", "ChatGPTRun add_tests",                 mode = mode, desc = "Add Tests" },
-                { "<leader>co", "ChatGPTRun optimize_code",             mode = mode, desc = "Optimize Code" },
-                { "<leader>cs", "ChatGPTRun summarize",                 mode = mode, desc = "Summarize" },
-                { "<leader>cf", "ChatGPTRun fix_bugs",                  mode = mode, desc = "Fix Bugs" },
-                { "<leader>cx", "ChatGPTRun explain_code",              mode = mode, desc = "Explain Code" },
-                { "<leader>cr", "ChatGPTRun roxygen_edit",              mode = mode, desc = "Roxygen Edit" },
-                { "<leader>cl", "ChatGPTRun code_readability_analysis", mode = mode, desc = "Code Readability Analysis" },
+                api_key_cmd = "pass show personal/openai.com/apikey"
             }
-        end,
-        config = true,
+        end
     }
 }
