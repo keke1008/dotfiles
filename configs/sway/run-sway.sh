bfec83e0a38788b6d1aaf366cf47f082c9d3919e
keke1008 <keke2222337@gmail.com>
2024-03-27 16:45:10 +0900

    fix(sway): env

--- configs/sway/run-sway.sh
+++ configs/sway/run-sway.sh
@@ -2,7 +2,9 @@
 # Use in /usr/share/wayland-sessions/sway.desktop
 
 export XMODIFIERS=@im=fcitx
-export GTK_IM_MODULE=wayland
 export QT_IM_MODULE=fcitx
+
+# https://qiita.com/aratetsu_sp2/items/6bd89e5959ba54ede391#fn-2
+export GTK_IM_MODULE=fcitx
 
 exec sway
