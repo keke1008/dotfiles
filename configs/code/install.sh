dec6d10f9d577421df31e30593db7f0111a33d07
keke1008 <keke2222337@gmail.com>
2024-04-17 12:56:07 +0900

    fix(code): fix install script

--- configs/code/install.sh
+++ configs/code/install.sh
@@ -4,7 +4,7 @@ CODE_DIR="$XDG_CONFIG_HOME/Code - OSS/User"
 
 if [ ! -d "$CODE_DIR" ]; then
     echo "Code is not installed. Cancel creating a symlink." 1>&2
-    return 0
+    exit 0
 fi
 
 ln -snvf "$DOTPATH/configs/code/keybindings.json" "$CODE_DIR/keybindings.json"
