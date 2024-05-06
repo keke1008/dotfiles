deb6c9adee47208072ed50bdb0eb8d1a176cec93
keke1008 <keke2222337@gmail.com>
2024-05-06 17:42:25 +0900

    feat(code): add settings.json

--- configs/code/install.sh
+++ configs/code/install.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -eu
 
-CODE_DIR="$XDG_CONFIG_HOME/Code - OSS/User"
+SRC_DIR="$DOTPATH/configs/code"
+CODE_OSS_DIR="$XDG_CONFIG_HOME/Code - OSS/User"
+VSCODE_DIR="$XDG_CONFIG_HOME/Code/User"
 
-if [ ! -d "$CODE_DIR" ]; then
-    echo "Code is not installed. Cancel creating a symlink." 1>&2
-    exit 0
-fi
-
-ln -snvf "$DOTPATH/configs/code/keybindings.json" "$CODE_DIR/keybindings.json"
+for CODE_DIR in "$CODE_OSS_DIR" "$VSCODE_DIR"; do
+	mkdir -p "$CODE_DIR"
+	ln -snvf "$SRC_DIR/keybindings.json" "$CODE_DIR"
+	ln -snvf "$SRC_DIR/settings.json" "$CODE_DIR"
+done
