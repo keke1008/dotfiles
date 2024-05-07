28039892a20068e2a859701462d7b718f3bfc556
keke1008 <keisuke-goto@freee.co.jp>
2024-05-07 16:59:20 +0900

    feat(code): update install.script

--- configs/code/install.sh
+++ configs/code/install.sh
@@ -1,11 +1,25 @@
 #!/bin/sh -eu
 
 SRC_DIR="$DOTPATH/configs/code"
-CODE_OSS_DIR="$XDG_CONFIG_HOME/Code - OSS/User"
-VSCODE_DIR="$XDG_CONFIG_HOME/Code/User"
+
+if [ "$(uname)" == "Darwin" ]; then
+	CODE_BASE_DIR="$HOME/Library/Application Support/"
+	CODE_OSS_DIR="$CODE_BASE_DIR/Code/User"
+	VSCODE_DIR="$CODE_BASE_DIR/Code - OSS/User"
+else
+	CODE_OSS_DIR="$XDG_CONFIG_HOME/Code - OSS/User"
+	VSCODE_DIR="$XDG_CONFIG_HOME/Code/User"
+fi
 
 for CODE_DIR in "$CODE_OSS_DIR" "$VSCODE_DIR"; do
 	mkdir -p "$CODE_DIR"
 	ln -snvf "$SRC_DIR/keybindings.json" "$CODE_DIR"
 	ln -snvf "$SRC_DIR/settings.json" "$CODE_DIR"
 done
+
+# for remote development
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+	VSCODE_SERVER_DIR="$HOME/.vscode-server/data/Machine"
+	mkdir -p "$VSCODE_SERVER_DIR"
+	ln -snvf "$SRC_DIR/settings.json" "$VSCODE_SERVER_DIR"
+fi
