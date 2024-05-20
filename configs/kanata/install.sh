3dc659116550da7bea4fae624701a2095abf33f6
keke1008 <keisuke-goto@freee.co.jp>
2024-05-20 16:30:30 +0900

    fix: kanata install script

--- configs/kanata/install.sh
+++ configs/kanata/install.sh
@@ -2,7 +2,7 @@
 
 if ! command -v systemctl >/dev/null 2>&1; then
 	echo "systemctl command not found. Kanata installation canceled."
-	exit
+	return
 fi
 
 service_file_path="$DOTPATH/configs/kanata/kanata.service"
@@ -13,7 +13,7 @@ ln -snfv "$service_file_path" "$link_dir_path"
 
 if ! command -v kanata >/dev/null 2>&1; then
 	echo "kanata command not found. Enable systemd service canceled."
-	exit
+	return
 fi
 
 systemctl --user daemon-reload
