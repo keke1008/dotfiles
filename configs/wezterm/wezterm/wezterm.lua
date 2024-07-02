6807770d11e3f54d3391b851dbff028e5ce6b1c4
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-07-02 09:22:52 +0900

    feat(wezterm): update config

--- configs/wezterm/wezterm/wezterm.lua
+++ configs/wezterm/wezterm/wezterm.lua
@@ -5,6 +5,12 @@ config.font = wezterm.font "UDEV Gothic 35NF"
 config.color_scheme = "tokyonight_night"
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
+config.window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0,
+}
 config.hide_tab_bar_if_only_one_tab = true
 
 return config
