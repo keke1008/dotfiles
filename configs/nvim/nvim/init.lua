89c6d2d9143c00ce3646ba0ab0cca34f556f7384
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-08-14 22:25:33 +0900

    feat(nvim): update config

--- configs/nvim/nvim/init.lua
+++ configs/nvim/nvim/init.lua
@@ -32,7 +32,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.go", "*.sh" },
+    pattern = { "*.go", "*.sh", "*.asm" },
     callback = function() vim.bo.expandtab = false end,
 })
 
