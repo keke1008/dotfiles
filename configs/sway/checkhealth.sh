0f97172f9d7763a3ab6dd55fca5a0219c8516b78
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-08-15 22:27:42 +0900

    feat(sway): add screen locker config

--- configs/sway/checkhealth.sh
+++ configs/sway/checkhealth.sh
@@ -1,19 +1,27 @@
 #!/bin/bash -eu
 
-report_file_readable "info" "${DOTFILES_LOCAL_HOME}/sway/local_config" "Local sway config file"
+main() {
+	report_file_readable "info" "${DOTFILES_LOCAL_HOME}/sway/local_config" "Local sway config file"
 
-if command -v sway >/dev/null 2>&1; then
-	level="error"
-else
-	level="info"
-fi
+	if command -v sway >/dev/null 2>&1; then
+		local level_error="error"
+		local level_warn="warn"
+	else
+		local level_error="info"
+		local level_warn="info"
+	fi
 
-report_command_exists "info" "sway"
-report_command_exists "$level" "waybar" "Status bar"
-report_command_exists "$level" "wofi" "Application launcher"
-report_command_exists "$level" "alacritty" "Terminal emulator"
+	report_command_exists "info" "sway"
+	report_command_exists "${level_error}" "waybar" "Status bar"
+	report_command_exists "${level_error}" "wofi" "Application launcher"
+	report_command_exists "${level_error}" "alacritty" "Terminal emulator"
 
-report_command_exists "warn" "dunst" "Notification daemon"
-report_command_exists "warn" "fcitx5" "Input method"
+	report_command_exists "${level_warn}" "dunst" "Notification daemon"
+	report_command_exists "${level_warn}" "swaybg" "Wallpaper setter"
+	report_command_exists "${level_warn}" "swaylock" "Screen locker"
+	report_command_exists "${level_warn}" "swayidle" "Idle manager"
 
-report_command_exists "info" "grim" "Screenshot utility"
+	report_command_exists "info" "grim" "Screenshot utility"
+}
+
+main
