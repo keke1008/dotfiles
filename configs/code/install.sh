53928d92f8c9572890a66a872717948373b9f7a7
keke1008 <keisuke-goto@freee.co.jp>
2024-09-09 10:37:57 +0900

    feat(code): cursor support

--- configs/code/install.sh
+++ configs/code/install.sh
@@ -8,7 +8,7 @@ main() {
 	fi
 
 	local code_dirname
-	for code_dirname in "Code - OSS" "Code"; do
+	for code_dirname in "Code - OSS" "Code" "Cursor"; do
 		local code_dir="${code_base_dir}/${code_dirname}/User"
 		mkdir -p "${code_dir}"
 		declare_config_link "${code_dir}/keybindings.json" "keybindings.json" "${code_dirname}-keybindings.json"
@@ -17,9 +17,11 @@ main() {
 
 	# for remote development
 	if [ -n "${SSH_CLIENT:-''}" ] || [ -n "${SSH_TTY:-''}" ]; then # Use parameter expansion to prevent an 'undefined variable' error
-		local vscode_server_dir="${HOME}/.vscode-server/data/Machine"
-		mkdir -p "${vscode_server_dir}"
-		declare_config_link "${vscode_server_dir}/settings.json" "settings.json" "remote-settings.json"
+		for server_dirname in ".vscode-server" ".cursor-server"; do
+			local server_dir="${HOME}/${server_dirname}/data/Machine"
+			mkdir -p "${server_dir}"
+			declare_config_link "${server_dir}/settings.json" "settings.json" "remote-settings.json"
+		done
 	fi
 }
 
