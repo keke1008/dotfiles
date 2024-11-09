9b0025e5a2dcf1e9d78148b90f95d5c6f525491e
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-11-09 15:15:59 +0900

    feat(fish): update installation script

--- configs/fish/install.sh
+++ configs/fish/install.sh
@@ -1,21 +1,23 @@
 #!/bin/sh -eu
 
-main() {
-	mkdir -p "${XDG_CONFIG_HOME}/fish"
-	declare_config_link "${XDG_CONFIG_HOME}/fish/config.fish" "config.fish"
-	declare_config_link "${XDG_CONFIG_HOME}/fish/fish_plugins" "fish_plugins"
+declare_xdg_config_link
 
-	local dir
-	for dir in "functions" "themes"; do
-		mkdir -p "${XDG_CONFIG_HOME}/fish/${dir}"
-
-		local file
-		for file in $(find "${DOTPATH}/configs/fish/${dir}" -type f); do
-			local name
-			name="$(basename "${file}")"
-			declare_config_link "${XDG_CONFIG_HOME}/fish/${dir}/${name}" "${dir}/${name}"
-		done
-	done
-}
-
-main
+# main() {
+# 	mkdir -p "${XDG_CONFIG_HOME}/fish"
+# 	declare_config_link "${XDG_CONFIG_HOME}/fish/config.fish" "config.fish"
+# 	declare_config_link "${XDG_CONFIG_HOME}/fish/fish_plugins" "fish_plugins"
+#
+# 	local dir
+# 	for dir in "functions" "themes"; do
+# 		mkdir -p "${XDG_CONFIG_HOME}/fish/${dir}"
+#
+# 		local file
+# 		for file in $(find "${DOTPATH}/configs/fish/${dir}" -type f); do
+# 			local name
+# 			name="$(basename "${file}")"
+# 			declare_config_link "${XDG_CONFIG_HOME}/fish/${dir}/${name}" "${dir}/${name}"
+# 		done
+# 	done
+# }
+#
+# main
