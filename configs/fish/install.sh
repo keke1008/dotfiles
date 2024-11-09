6f367ab802524abf4519ee62e3ab877f46844efc
keke1008 <49900846+keke1008@users.noreply.github.com>
2024-11-10 01:20:10 +0900

    refactor(fish): remove comment

--- configs/fish/install.sh
+++ configs/fish/install.sh
@@ -1,23 +1,3 @@
 #!/bin/sh -eu
 
 declare_xdg_config_link
-
-# main() {
-# 	mkdir -p "${XDG_CONFIG_HOME}/fish"
-# 	declare_config_link "${XDG_CONFIG_HOME}/fish/config.fish" "config.fish"
-# 	declare_config_link "${XDG_CONFIG_HOME}/fish/fish_plugins" "fish_plugins"
-#
-# 	local dir
-# 	for dir in "functions" "themes"; do
-# 		mkdir -p "${XDG_CONFIG_HOME}/fish/${dir}"
-#
-# 		local file
-# 		for file in $(find "${DOTPATH}/configs/fish/${dir}" -type f); do
-# 			local name
-# 			name="$(basename "${file}")"
-# 			declare_config_link "${XDG_CONFIG_HOME}/fish/${dir}/${name}" "${dir}/${name}"
-# 		done
-# 	done
-# }
-#
-# main
