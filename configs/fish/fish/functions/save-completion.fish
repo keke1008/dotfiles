e489596850302fd728dd151c60cfd47986a04100
keke1008 <keisuke-goto@freee.co.jp>
2024-12-04 11:55:55 +0900

    fix(fish): save-completion

--- configs/fish/fish/functions/save-completion.fish
+++ configs/fish/fish/functions/save-completion.fish
@@ -1,4 +1,8 @@
 function save-completion
     set command $argv[1]
-    eval $argv >$XDG_DATA_HOME/fish/vendor_completions.d/$command.fish
+
+    set completions_dir $XDG_DATA_HOME/fish/vendor_completions.d
+    mkdir -p $completions_dir
+
+    eval $argv >$completions_dir/$command.fish
 end
