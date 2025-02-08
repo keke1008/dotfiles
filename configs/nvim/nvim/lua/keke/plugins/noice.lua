e82760341df52493f3490fa84dd7b15c5f7e0548
keke1008 <49900846+keke1008@users.noreply.github.com>
2025-02-08 15:16:38 +0900

    feat(nvim): update notify style

--- configs/nvim/nvim/lua/keke/plugins/noice.lua
+++ configs/nvim/nvim/lua/keke/plugins/noice.lua
@@ -1,85 +1,93 @@
 return {
-    "folke/noice.nvim",
-    dependencies = {
-        "MunifTanjim/nui.nvim",
+    {
         "rcarriga/nvim-notify",
+        opts = {
+            render = "wrapped-compact",
+        },
     },
-    keys = function()
-        return {
-            {
-                "<leader><leader>",
-                function()
-                    require("notify").dismiss({})
-                    vim.cmd("noh")
-                end,
-                mode = "n",
-                desc = "dimiss notifications and highlights",
-            },
-            {
-                "<C-f>",
-                function()
-                    if not require("noice.lsp").scroll(4) then
-                        return "<C-f>"
-                    end
-                end,
-                silent = true,
-                expr = true,
-                mode = "n",
-                desc = "Scroll down noice lsp",
-            },
-            {
-                "<C-b>",
-                function()
-                    if not require("noice.lsp").scroll(-4) then
-                        return "<C-f>"
-                    end
-                end,
-                silent = true,
-                expr = true,
-                mode = "n",
-                desc = "Scroll up noice lsp",
-            },
-        }
-    end,
-    config = function()
-        require("noice").setup({
-            commands = {
-                history = {
-                    view = "vsplit",
-                },
-            },
-            views = {
-                cmdline_popup = {
-                    position = { row = "20%" },
-                    zindex = 100,
-                },
-                confirm = {
-                    position = { row = "20%" },
-                },
-                hover = {
-                    border = { style = "rounded" },
-                    position = { row = 2 },
-                },
-            },
-            routes = {
+    {
+        "folke/noice.nvim",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        keys = function()
+            return {
                 {
-                    view = "mini",
-                    filter = {
-                        any = {
-                            { warning = true },
-                            { event = "notify", kind = "info" },
-                            {
-                                event = "msg_show",
-                                any = {
-                                    { kind = "emsg", find = "^E486" },
-                                    { kind = "emsg", find = "^E37" },
-                                    { kind = "", find = "written$" },
+                    "<leader><leader>",
+                    function()
+                        require("notify").dismiss({})
+                        vim.cmd("noh")
+                    end,
+                    mode = "n",
+                    desc = "dimiss notifications and highlights",
+                },
+                {
+                    "<C-f>",
+                    function()
+                        if not require("noice.lsp").scroll(4) then
+                            return "<C-f>"
+                        end
+                    end,
+                    silent = true,
+                    expr = true,
+                    mode = "n",
+                    desc = "Scroll down noice lsp",
+                },
+                {
+                    "<C-b>",
+                    function()
+                        if not require("noice.lsp").scroll(-4) then
+                            return "<C-f>"
+                        end
+                    end,
+                    silent = true,
+                    expr = true,
+                    mode = "n",
+                    desc = "Scroll up noice lsp",
+                },
+            }
+        end,
+        config = function()
+            require("noice").setup({
+                commands = {
+                    history = {
+                        view = "vsplit",
+                    },
+                },
+                views = {
+                    cmdline_popup = {
+                        position = { row = "20%" },
+                        zindex = 100,
+                    },
+                    confirm = {
+                        position = { row = "20%" },
+                    },
+                    hover = {
+                        border = { style = "rounded" },
+                        position = { row = 2 },
+                    },
+                },
+                routes = {
+                    {
+                        view = "mini",
+                        filter = {
+                            any = {
+                                { warning = true },
+                                { event = "notify", kind = "info" },
+                                {
+                                    event = "msg_show",
+                                    any = {
+                                        { kind = "emsg", find = "^E486" },
+                                        { kind = "emsg", find = "^E37" },
+                                        { kind = "", find = "written$" },
+                                    },
                                 },
                             },
                         },
                     },
                 },
-            },
-        })
-    end,
+            })
+        end,
+    },
 }
