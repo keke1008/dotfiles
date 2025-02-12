0111fa41a1cffdcc685bbe5cbf26d0561df0f247
keke1008 <49900846+keke1008@users.noreply.github.com>
2025-02-13 00:27:20 +0900

    feat(hyprland): add some programs

--- configs/hyprland/checkhealth.sh
+++ configs/hyprland/checkhealth.sh
@@ -1,3 +1,20 @@
 #!/bin/sh -eu
 
-report_file_readable "info" "${DOTFILES_LOCAL_HOME}/hyprland/hyprland.conf" "Local config file"
+main() {
+	report_file_readable "info" "${DOTFILES_LOCAL_HOME}/hyprland/hyprland.conf" "Local config file"
+	report_command_exists "info" "hyprctl" "Hyprland WM"
+
+	if command -v hyprctl >/dev/null 2>&1; then
+		local level_error="error"
+		local level_warn="warn"
+	else
+		local level_error="info"
+		local level_warn="info"
+	fi
+
+	report_command_exists "${level_warn}" hyprpanel "Status bar"
+	report_command_exists "${level_warn}" hyprlock "Screen locker"
+	report_command_exists "${level_warn}" hypridle "Idle manager"
+}
+
+main
