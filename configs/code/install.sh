6899ba0ddf40128fb1f578021165aa2a0d4a76da
keke1008 <49900846+keke1008@users.noreply.github.com>
2025-04-22 16:38:27 +0900

    feat(code): do not install config for remote develpment

--- configs/code/install.sh
+++ configs/code/install.sh
@@ -15,14 +15,14 @@ main() {
 		declare_config_link "${code_dir}/settings.json" "settings.json" "${code_dirname}-settings.json"
 	done
 
-	# for remote development
-	if [ -n "${SSH_CLIENT:-''}" ] || [ -n "${SSH_TTY:-''}" ]; then # Use parameter expansion to prevent an 'undefined variable' error
-		for server_dirname in ".vscode-server" ".cursor-server"; do
-			local server_dir="${HOME}/${server_dirname}/data/Machine"
-			mkdir -p "${server_dir}"
-			declare_config_link "${server_dir}/settings.json" "settings.json" "remote-settings.json"
-		done
-	fi
+	# # for remote development
+	# if [ -n "${SSH_CLIENT:-''}" ] || [ -n "${SSH_TTY:-''}" ]; then # Use parameter expansion to prevent an 'undefined variable' error
+	# 	for server_dirname in ".vscode-server" ".cursor-server"; do
+	# 		local server_dir="${HOME}/${server_dirname}/data/Machine"
+	# 		mkdir -p "${server_dir}"
+	# 		declare_config_link "${server_dir}/settings.json" "settings.json" "remote-settings.json"
+	# 	done
+	# fi
 }
 
 main
