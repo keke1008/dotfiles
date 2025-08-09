9f91e92df138be979eefa6f5418f7491df2544f2
keke1008 <49900846+keke1008@users.noreply.github.com>
2025-08-09 18:07:46 +0900

    feat(macos): add backslash setup

--- configs/macos/init.sh
+++ configs/macos/init.sh
@@ -24,11 +24,15 @@ defaults write 'com.apple.dock' autohide -int 1
 # Enable dark-mode
 defaults write 'Apple Global Domain' AppleInterfaceStyle -string Dark
 
+# Enable backslash input when the yen symbol key is pressed.
+defaults write 'com.apple.inputmethod.Kotoeri' JIMPrefCharacterForYenKey -int 1
+
 log "info" "Reloading settings..."
 /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
 
 log "info" "macOS settings have been applied."
 
 log "info" "Please manually set the following settings in System Preferences:"
-log "info" "  - Set Ctrl+Option+Space to switch input sources in Keyboard settings."
 log "info" "  - Disable Ctrl-Space in Keyboard settings."
+log "info" "  - Enable '英字' input mode of '日本語 - ローマ字入力' input source."
+log "info" "  - Remove 'ABC' input source for typing backslash."
