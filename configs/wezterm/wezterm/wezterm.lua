4ef7fe703a71ee90a2de2b940d4f9bcc1744f282
keke1008 <49900846+keke1008@users.noreply.github.com>
2025-08-09 14:43:57 +0900

    feat(wezterm): remove font config

--- configs/wezterm/wezterm/wezterm.lua
+++ configs/wezterm/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
-config.font = wezterm.font "UDEV Gothic 35NF"
 config.color_scheme = "tokyonight_night"
 config.window_background_opacity = 0.9
 config.window_decorations = "RESIZE"
