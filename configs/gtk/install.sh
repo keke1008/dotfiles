041b423f571e07f8314e82543596d0127012364d
keke1008 <49900846+keke1008@users.noreply.github.com>
2026-04-19 00:39:06 +0900

    feat(gtk,qt): remove configuration

--- configs/gtk/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -eu
-
-for version in 3.0 4.0; do
-	mkdir -p "$XDG_CONFIG_HOME/gtk-${version}"
-	declare_xdg_config_link \
-		"gtk-${version}-settings.ini" \
-		"gtk-${version}/settings.ini"
-done
--- configs/qt/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh -eu
-
-declare_xdg_config_link "qt5ct.conf" "qt5ct/qt5ct.conf"
