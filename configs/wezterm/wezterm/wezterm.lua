dc52e94bab7cbec411ff769e752464919c6b8907
keke1008 <49900846+keke1008@users.noreply.github.com>
2026-06-12 22:54:00 +0900

    feat(wezterm): update tab config

--- configs/wezterm/wezterm/wezterm.lua
+++ configs/wezterm/wezterm/wezterm.lua
@@ -1,15 +1,98 @@
 local wezterm = require "wezterm"
 local config = wezterm.config_builder()
 
-config.color_scheme = "tokyonight_night"
-config.window_background_opacity = 0.9
-config.window_decorations = "RESIZE"
+config.keys = {
+  {
+    key = "r",
+    mods = "CMD|SHIFT",
+    action = wezterm.action.ReloadConfiguration,
+  },
+}
+
 config.window_padding = {
     left = 0,
     right = 0,
-    top = 0,
-    bottom = 0,
+    top = 8,
+    bottom = 0
 }
+
+config.window_background_opacity = 0.9
+config.window_decorations = "RESIZE"
+config.color_scheme = "blue"
 config.hide_tab_bar_if_only_one_tab = true
+config.use_fancy_tab_bar = false
+config.tab_max_width = 40
+
+-- Setup tab style
+do
+    local function retain_table(elements)
+        local result = {}
+
+        for _, e in ipairs(elements) do
+            if type(e) == "table" then
+                table.insert(result, e)
+            end
+        end
+
+        return result
+    end
+
+    local SEPARATOR = wezterm.nerdfonts.pl_left_soft_divider
+
+    wezterm.on(
+        "format-tab-title",
+        function(tab, tabs, panes, config, hover, max_width)
+            local tab_title = tab.tab_title or ""
+            local title = tostring(tab.tab_index) .. (#tab_title == 0 and "" or " " .. tab_title)
+
+            return retain_table({
+                tab.tab_index == 0 and { Text = ' WezTerm ' .. SEPARATOR },
+
+                { Text = " " },
+                (tab.is_active or hover) and { Attribute = { Intensity = "Bold" } },
+                (tab.is_active or hover) and { Attribute = { Underline = "Single" } },
+                { Text = title },
+                { Attribute = { Underline = "None" } },
+
+                { Text = tab.is_active and " * " or " - " },
+                { Text = SEPARATOR }
+            })
+        end
+    )
+end
+
+-- Load local config file
+do
+    local function get_local_config_path()
+        local xdg_config_home = nil
+        xdg_config_home = os.getenv("XDG_CONFIG_HOME")
+        if xdg_config_home == nil then
+            xdg_config_home = os.getenv("HOME") .. "/.config"
+        end
+
+        return xdg_config_home .. "/dotfiles/local/wezterm/wezterm.lua"
+    end
+
+    local function load_local_config(config)
+        local local_config_path = get_local_config_path()
+        local file = io.open(local_config_path, "r")
+        if file == nil then
+            return
+        end
+
+        local local_config_str = file:read("*all")
+        file:close()
+
+
+        local local_config = assert(load(local_config_str))()
+        if type(local_config) ~= "function" then
+            error(local_config_path .. " does not return function")
+        end
+
+        local_config(config)
+    end
+
+    load_local_config(config)
+end
 
 return config
