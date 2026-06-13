73343ea26be96c2b0a11ac6720afbee077dcfeff
keke1008 <49900846+keke1008@users.noreply.github.com>
2026-06-13 20:08:16 +0900

    feat(nvim): local config

--- configs/nvim/nvim/init.lua
+++ configs/nvim/nvim/init.lua
@@ -12,3 +12,11 @@ require("keke.setup")
 if not require("keke.lazy").load().loaded then
     require("keke.setup_noplugin")
 end
+
+-- load local config
+do
+    local xdg_config_home = os.getenv("XDG_CONFIG_HOME") or (os.getenv("HOME") .. "/.config")
+    local local_config_path = xdg_config_home .. "/dotfiles/local/nvim"
+    vim.opt.runtimepath:append(local_config_path)
+    pcall(require, "keke_local")
+end
