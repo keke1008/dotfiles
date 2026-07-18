eb00ba39e995cb72bc788d17192e8f1c3db8f747
keke1008 <49900846+keke1008@users.noreply.github.com>
2026-07-18 22:10:02 +0900

    feat: remove nix flake dependency

--- flake.nix
+++ flake.nix
@@ -1,51 +1,54 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells = {
-          default = pkgs.mkShell {
-            packages = with pkgs; [
-              bash-language-server
-              lua-language-server
-              luajitPackages.luacheck
-              nil
-              biome
-              kdePackages.qtdeclarative # for qmlls
-              selene
-              shellcheck
-              shfmt
-              stylua
-              taplo
-              typescript-language-server
-              typescript # for typescript-language-server
-              vscode-json-languageserver
-            ];
-          };
-          initialize = pkgs.mkShell {
-            packages = with pkgs; [
-              curl
-              fish
-              git
-              neovim
-              tmux
-              zsh
-            ];
-          };
-        };
-      }
-    )
-    // {
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      lib = nixpkgs.lib;
+      forAllSystems = f: lib.genAttrs systems (system: f nixpkgs.legacyPackages."${system}");
+    in
+    {
       nixosModules = ./nix/nixosModules;
       homeModules = ./nix/homeModules;
+
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            bash-language-server
+            bats
+            lua-language-server
+            luajitPackages.luacheck
+            nil
+            biome
+            kdePackages.qtdeclarative # for qmlls
+            selene
+            shellcheck
+            shfmt
+            stylua
+            taplo
+            typescript-language-server
+            typescript # for typescript-language-server
+            vscode-json-languageserver
+          ];
+        };
+
+        initialize-test = pkgs.mkShell {
+          packages = with pkgs; [
+            curl
+            fish
+            git
+            neovim
+            tmux
+            zsh
+          ];
+        };
+      });
     };
 }
