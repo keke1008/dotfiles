dd1546b667a01b641976f2237032929b4663d28a
keke1008 <49900846+keke1008@users.noreply.github.com>
2026-08-25 09:37:44 +0900

    feat(nvim): netrw enhancement

--- configs/nvim/nvim/lua/keke/setup_noplugin/netrw.lua
+++ configs/nvim/nvim/lua/keke/setup_noplugin/netrw.lua
@@ -2,7 +2,7 @@ vim.cmd("packadd netrw")
 
 vim.g.netrw_banner = 0 -- Hide banner
 vim.g.netrw_liststyle = 3 -- Tree format
-vim.g.netrw_winsize = 40
+vim.g.netrw_winsize = -40 -- Negative value specifies the number of columns
 
 vim.keymap.set("n", "<Space>se", "<CMD>Lexplore<CR>")
 vim.keymap.set("n", "<Space>shl", "<CMD>Lexplore<CR>")
